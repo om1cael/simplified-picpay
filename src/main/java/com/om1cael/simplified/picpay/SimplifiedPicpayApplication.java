74c0db7c7f60ee83cb601af49a453eaf7a51eaad
om1cael <188113439+om1cael@users.noreply.github.com>
2025-03-23 12:35:27 -0300

    Initial commit

---
+++ src/main/java/com/om1cael/simplified/picpay/SimplifiedPicpayApplication.java
@@ -0,0 +1,13 @@
+package com.om1cael.simplified.picpay;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimplifiedPicpayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimplifiedPicpayApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/om1cael/simplified/picpay/SimplifiedPicpayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.om1cael.simplified.picpay;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimplifiedPicpayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
