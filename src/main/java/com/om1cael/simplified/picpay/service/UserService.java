2fbbe48884c8a51ffaa98a54236c9b349c9ca229
om1cael <188113439+om1cael@users.noreply.github.com>
2025-03-23 13:53:04 -0300

    feat: set balance when creating a new user

--- src/main/java/com/om1cael/simplified/picpay/service/UserService.java
+++ src/main/java/com/om1cael/simplified/picpay/service/UserService.java
@@ -2,6 +2,7 @@ package com.om1cael.simplified.picpay.service;
 
 import com.om1cael.simplified.picpay.model.User;
 import com.om1cael.simplified.picpay.repository.UserRepository;
+import jakarta.persistence.EntityNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -11,6 +12,12 @@ public class UserService {
     private UserRepository userRepository;
 
     public User create(User user) {
+        user.setBalance(500.0);
         return userRepository.save(user);
+    }
+
+    public User getByCpf(String cpf) {
+        return userRepository.findByCpf(cpf)
+                .orElseThrow(() -> new EntityNotFoundException("User not found"));
     }
 }
