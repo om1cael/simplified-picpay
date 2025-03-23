a8508f03837d90b89014691e637d2ad83c29a3ca
om1cael <188113439+om1cael@users.noreply.github.com>
2025-03-23 12:58:37 -0300

    fix: user repository find by cpf

--- src/main/java/com/om1cael/simplified/picpay/repository/UserRepository.java
+++ src/main/java/com/om1cael/simplified/picpay/repository/UserRepository.java
@@ -2,9 +2,11 @@ package com.om1cael.simplified.picpay.repository;
 
 import com.om1cael.simplified.picpay.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
+@Repository
 public interface UserRepository extends JpaRepository<User, Long> {
-    Optional<User> findByCPF(String cpf);
+    Optional<User> findByCpf(String cpf);
 }
